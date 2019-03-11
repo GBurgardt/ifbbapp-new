f69de8ba7147abc4a3f41f85a882ef03468c5a9f
francosili <fsili72@gmail.com>
2019-03-11 20:50:31 -0300

    Una banda de cosas

---
+++ src/pages/inscripciones/inscripciones.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-inscripciones',
+  templateUrl: 'inscripciones.html',
+})
+export class InscripcionesPage {
+
+  item:any;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.get('item');
+  }
+
+  ionViewDidLoad() {
+    console.log(this.item);
+  }
+
+}
