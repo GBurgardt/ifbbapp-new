82fc77d66262e57dbb10710e717d0e59973f4b95
Cristian Martinez <martinezdurbanocristian@hotmail.com>
2019-07-01 17:48:14 -0300

    Primer approach a InfinitScroll. He fallado miserablemente

--- src/pages/salud-bienestar/salud-bienestar.ts
+++ src/pages/salud-bienestar/salud-bienestar.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { NewsService } from '../../services/newsService/newsService';
 import { SaludBienestarService } from '../../services/saludBienestarService/saludBienestarService';
 
+
 /**
  * Generated class for the SaludBienestarPage page.
  *
@@ -15,7 +16,7 @@ import { SaludBienestarService } from '../../services/saludBienestarService/salu
 // Definir clase "noticias"
 // Qué hago en caso de error con getNews?
 
-@Component({  
+@Component({
   selector: 'page-salud-bienestar',
   templateUrl: 'salud-bienestar.html',
 })
@@ -36,11 +37,34 @@ export class SaludBienestarPage {
         resp => {
           if (resp.control.codigo === 'OK') {
             this.saludBienestar = resp.arraydatos;
+            console.log(this.saludBienestar);
           } else {
             // this.showAlert("Error", resp.descripcion)
           }
         }
       )
   }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+    console.log(this.saludBienestar);
+
+    setTimeout(() => {
+      // this.saludBienestarService.getSaludBienestar()
+      //   .then(
+      //     resp => {
+      //       if (resp.control.codigo === 'OK') {
+      //         this.saludBienestar = resp.arraydatos;        // Va un push acá, no un seteo
+      //       } else {
+      //         // this.showAlert("Error", resp.descripcion)
+      //       }
+      //     }
+      //   )
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
+  }
+  
 
 }
