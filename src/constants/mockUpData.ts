2439b3f774499a0f5c26237e42f99d06c455fbd8
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2019-10-24 14:45:49 -0300

    actulise news

--- src/constants/mockUpData.ts
+++ src/constants/mockUpData.ts
@@ -1,123 +1,133 @@
 export const mockSaludBienestar = [
     {
-        imagen: '../../assets/bottom-tabs/01-salud-bienestar/img02.jpg',
-        titulo: 'Desayuno',
-        subTitulo: 'Tostadas con huevos revueltos ',
+        imagen: "../../assets/bottom-tabs/01-salud-bienestar/img02.jpg",
+        titulo: "Desayuno",
+        subTitulo: "Tostadas con huevos revueltos "
     },
     {
-        imagen: '../../assets/bottom-tabs/01-salud-bienestar/img06.jpg',
-        titulo: 'Media mañana',
-        subTitulo: 'Batido de proteínas',
+        imagen: "../../assets/bottom-tabs/01-salud-bienestar/img06.jpg",
+        titulo: "Media mañana",
+        subTitulo: "Batido de proteínas"
     },
     {
-        imagen: '../../assets/bottom-tabs/01-salud-bienestar/img01.jpg',
-        titulo: 'Almuerzo',
-        subTitulo: 'Carne Burgardt',
+        imagen: "../../assets/bottom-tabs/01-salud-bienestar/img01.jpg",
+        titulo: "Almuerzo",
+        subTitulo: "Carne Burgardt"
     },
     {
-        imagen: '../../assets/bottom-tabs/01-salud-bienestar/img05.png',
-        titulo: 'Media tarde',
-        subTitulo: 'Queso crema francés',
+        imagen: "../../assets/bottom-tabs/01-salud-bienestar/img05.png",
+        titulo: "Media tarde",
+        subTitulo: "Queso crema francés"
     },
     {
-        imagen: '../../assets/bottom-tabs/01-salud-bienestar/img03.jpg',
-        titulo: 'Cena',
-        subTitulo: 'Express de Proteínas',
-    },
+        imagen: "../../assets/bottom-tabs/01-salud-bienestar/img03.jpg",
+        titulo: "Cena",
+        subTitulo: "Express de Proteínas"
+    }
 ];
 
 export const mockNews = [
     {
-        imagen: '../../assets/bottom-tabs/02-news/img04.png',
-        titulo: 'Fase 1: Semanas 1 y 2',
-        subTitulo: 'ENTRENAMIENTO 1: LUNES Y JUEVES (ENTRENAMIENTO DE EMPUJE)',
+        imagen: "../../assets/bottom-tabs/02-news/img01.png",
+        titulo: "IFBB SOUTH PACIFIC OCEANIA CHAMPIONSHIPS 2019",
+        subTitulo:
+            "El pasado fin de semana en Tahití, Polinesia Francesa, se llevó a... "
     },
     {
-        imagen: '../../assets/bottom-tabs/02-news/img04.png',
-        titulo: 'Fase 1: Semanas 1 y 2',
-        subTitulo: 'ENTRENAMIENTO 2: MIÉRCOLES (Tren Inferior)',
+        imagen: "../../assets/bottom-tabs/02-news/img02.png",
+        titulo: "POWER GAMES",
+        subTitulo:
+            "Representantes de las cinco federaciones internacionales, Bodybuilding and..."
     },
     {
-        imagen: '../../assets/bottom-tabs/02-news/img04.png',
-        titulo: 'Fase 1: Semanas 1 y 2',
-        subTitulo: 'ENTRENAMIENTO 3: MARTES Y VIERNES (ENTRENAMIENTO DE TRACCIÓN)',
+        imagen: "../../assets/bottom-tabs/02-news/img03.png",
+        titulo: "THE 2019 IFBB SOUTH PACIFIC JUDGES MEETING IN TAHITI",
+        subTitulo:
+            "Con motivo del IFBB South Pacific 2019, el pasado 5 de octubre..."
     },
     {
-        imagen: '../../assets/bottom-tabs/02-news/img04.png',
-        titulo: 'Fase 1: Semanas 3-4',
-        subTitulo: 'ENTRENAMIENTO 1: LUNES Y JUEVES (ENTRENAMIENTO DE TRACCIÓN)',
+        imagen: "../../assets/bottom-tabs/02-news/img04.png",
+        titulo: "Fase 1: Semanas 3-4",
+        subTitulo: "ENTRENAMIENTO 1: LUNES Y JUEVES (ENTRENAMIENTO DE TRACCIÓN)"
     },
     {
-        imagen: '../../assets/bottom-tabs/02-news/img05.png',
-        titulo: 'Fase 2: Semanas 3-4',
-        subTitulo: 'ENTRENAMIENTO 2: MIERCOLES (Tren Inferior)',
+        imagen: "../../assets/bottom-tabs/02-news/img05.png",
+        titulo: "Fase 2: Semanas 3-4",
+        subTitulo: "ENTRENAMIENTO 2: MIERCOLES (Tren Inferior)"
     },
     {
-        imagen: '../../assets/bottom-tabs/02-news/img05.png',
-        titulo: 'Fase 2: Semanas 3-4',
-        subTitulo: 'ENTRENAMIENTO 3: MARTES Y VIERNES (ENTRENAMIENTO DE TRACCIÓN)',
-    },
+        imagen: "../../assets/bottom-tabs/02-news/img05.png",
+        titulo: "Fase 2: Semanas 3-4",
+        subTitulo:
+            "ENTRENAMIENTO 3: MARTES Y VIERNES (ENTRENAMIENTO DE TRACCIÓN)"
+    }
 ];
 
 export const mockTorneos = [
     {
-        imagen: '../../assets/bottom-tabs/03-torneos/img07.jpg',
-        titulo: 'Copa Sarandi',
-        fechaInicio: 'Sabado 14 de Septiembre',
-        lugar: 'Candelaria, Misiones',
+        imagen: "../../assets/bottom-tabs/03-torneos/img07.jpg",
+        titulo: "Copa Sarandi",
+        fechaInicio: "Sabado 14 de Septiembre",
+        lugar: "Candelaria, Misiones"
     },
     {
-        imagen: '../../assets/bottom-tabs/03-torneos/img08.jpg',
-        titulo: 'Copa Ciudad del Viso',
-        fechaInicio: 'Domingo 22 de Septiembre',
-        lugar: 'Del Viso, Buenos Aires ',
+        imagen: "../../assets/bottom-tabs/03-torneos/img08.jpg",
+        titulo: "Copa Ciudad del Viso",
+        fechaInicio: "Domingo 22 de Septiembre",
+        lugar: "Del Viso, Buenos Aires "
     },
     {
-        imagen: '../../assets/bottom-tabs/03-torneos/img09.jpg',
-        titulo: 'Copa Ciudad de la Yerba',
-        fechaInicio: 'Sabado 19 de Octubre',
-        lugar: 'Apostoles, Misiones',
+        imagen: "../../assets/bottom-tabs/03-torneos/img09.jpg",
+        titulo: "Copa Ciudad de la Yerba",
+        fechaInicio: "Sabado 19 de Octubre",
+        lugar: "Apostoles, Misiones"
     },
     {
-        imagen: '../../assets/bottom-tabs/03-torneos/img10.jpg',
-        titulo: 'Victory Classic',
-        fechaInicio: 'Sabado 19 de Octubre',
-        lugar: 'Gualeguay, Entre Rios',
-    },
+        imagen: "../../assets/bottom-tabs/03-torneos/img10.jpg",
+        titulo: "Victory Classic",
+        fechaInicio: "Sabado 19 de Octubre",
+        lugar: "Gualeguay, Entre Rios"
+    }
 ];
 
 export const mockPromos = [
     {
-        imagen: '../../assets/bottom-tabs/04-promos/img04.png',
-        titulo: 'La nueva definición de proteína',
-        subTitulo: '​LANDERFIT PREMIUM WHEY es una de las formas más avanzadas y eficaces de proteínas disponibles para apoyar el crecimiento muscular. LANDERFIT PREMIUM WHEY ha sido procesado a la perfección, eliminando tanto la lactosa como la grasa, y dando increíble mezcla instantánea y un sabor impecable. A través de un extenso proceso de control de calidad, nuestra fórmula exclusiva ofrece una de las fuentes de proteínas más fácilmente digeribles, más biodisponibles y más limpias del mercado.',
+        imagen: "../../assets/bottom-tabs/04-promos/img04.png",
+        titulo: "La nueva definición de proteína",
+        subTitulo:
+            "​LANDERFIT PREMIUM WHEY es una de las formas más avanzadas y eficaces de proteínas disponibles para apoyar el crecimiento muscular. LANDERFIT PREMIUM WHEY ha sido procesado a la perfección, eliminando tanto la lactosa como la grasa, y dando increíble mezcla instantánea y un sabor impecable. A través de un extenso proceso de control de calidad, nuestra fórmula exclusiva ofrece una de las fuentes de proteínas más fácilmente digeribles, más biodisponibles y más limpias del mercado."
     },
     {
-        imagen: '../../assets/bottom-tabs/04-promos/img05.png',
-        titulo: 'Soporte diario avanzado',
-        subTitulo: '​LANDERFIT LIPO REVOLUTION es un completo complejo quemador de grasa especialmente formulado para aumentar la actividad termogénica y el enfoque mental. Esta mezcla única ayudara a quemar grasa, reducir el apetito y dar un impulso de alta energía.',
+        imagen: "../../assets/bottom-tabs/04-promos/img05.png",
+        titulo: "Soporte diario avanzado",
+        subTitulo:
+            "​LANDERFIT LIPO REVOLUTION es un completo complejo quemador de grasa especialmente formulado para aumentar la actividad termogénica y el enfoque mental. Esta mezcla única ayudara a quemar grasa, reducir el apetito y dar un impulso de alta energía."
     },
     {
-        imagen: '../../assets/bottom-tabs/04-promos/img06.png',
-        titulo: 'Pasa al próximo nivel',
-        subTitulo: 'LANDERFIT MUSCLE PUMP PREWORKOUT HIGH-PERFORMANCE es solo para aquellos que buscan evolucionar sus entrenamientos. Con una mezcla especialmente formulada de ingredientes que trabajan sinérgicamente para ejercicios musculares intensos, energía permanente, y un entrenamiento como nunca lo has experimentado antes. ¡Este es el último preworkout que necesitas!',
-    },
+        imagen: "../../assets/bottom-tabs/04-promos/img06.png",
+        titulo: "Pasa al próximo nivel",
+        subTitulo:
+            "LANDERFIT MUSCLE PUMP PREWORKOUT HIGH-PERFORMANCE es solo para aquellos que buscan evolucionar sus entrenamientos. Con una mezcla especialmente formulada de ingredientes que trabajan sinérgicamente para ejercicios musculares intensos, energía permanente, y un entrenamiento como nunca lo has experimentado antes. ¡Este es el último preworkout que necesitas!"
+    }
 ];
 
 export const mockCursos = [
     {
-        imagen: '../../assets/bottom-tabs/05-cursos/img04.jpg',
-        titulo: 'TEMA I: Metabolismo y equilibro energético.',
-        subTitulo: 'Metabolismo, Valor Energético, Calorías, Vías para liberar energía, Gasto energético diario total o Metabolismo Total, Metabolismo Basal, Efecto Térmico de los Alimentos, Determinación del Peso, Composición Corporal.',
+        imagen: "../../assets/bottom-tabs/05-cursos/img04.jpg",
+        titulo: "TEMA I: Metabolismo y equilibro energético.",
+        subTitulo:
+            "Metabolismo, Valor Energético, Calorías, Vías para liberar energía, Gasto energético diario total o Metabolismo Total, Metabolismo Basal, Efecto Térmico de los Alimentos, Determinación del Peso, Composición Corporal."
     },
     {
-        imagen: '../../assets/bottom-tabs/05-cursos/img05.jpg',
-        titulo: 'TEMA II: Carbohidratos',
-        subTitulo: 'Definición de los Carbohidratos, Monosacáridos, Disacáridos, Polisacáridos, Fibra, Glucógeno, Rendimiento Atlético e Índice Glucémico.',
+        imagen: "../../assets/bottom-tabs/05-cursos/img05.jpg",
+        titulo: "TEMA II: Carbohidratos",
+        subTitulo:
+            "Definición de los Carbohidratos, Monosacáridos, Disacáridos, Polisacáridos, Fibra, Glucógeno, Rendimiento Atlético e Índice Glucémico."
     },
     {
-        imagen: '../../assets/bottom-tabs/05-cursos/img06.jpg',
-        titulo: 'TEMA III: Proteínas',
-        subTitulo: '¿Qué son?, Funciones, Necesidades diarias, Recomendaciones generales de consumo, Diferencias entre proteínas, Suplementos.',
-    },
+        imagen: "../../assets/bottom-tabs/05-cursos/img06.jpg",
+        titulo: "TEMA III: Proteínas",
+        subTitulo:
+            "¿Qué son?, Funciones, Necesidades diarias, Recomendaciones generales de consumo, Diferencias entre proteínas, Suplementos."
+    }
 ];
